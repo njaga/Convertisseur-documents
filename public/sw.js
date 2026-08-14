99f4146427ff8f3d7a911a39f494ebbed4f5e062
Ndiaga Ndiaye <njaga01@outlook.fr>
2026-08-14 13:15:39 +0000

    Stop precaching the retired OG image asset

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'doxali-shell-v1';
-const APP_SHELL = ['/', '/manifest.json', '/favicon.svg', '/og-image.png'];
+const CACHE_NAME = 'doxali-shell-v2';
+const APP_SHELL = ['/', '/manifest.json', '/favicon.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
